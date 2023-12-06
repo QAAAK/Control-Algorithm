9d3414617d0931cf766a71bdb0ce3106a6fa71d9
Dmitry Santalov <dmitsantalov@yandex.ru>
2023-12-06 17:19:54 +0300

    ver.1.4

---
+++ DDL Table/tbl_load_log.sql
@@ -0,0 +1,23 @@
+-- meta_info.tbl_load_log определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.tbl_load_log;
+
+CREATE TABLE meta_info.tbl_load_log (
+	id int8 NOT NULL DEFAULT nextval('meta_info.tbl_load_update_id_seq'::regclass),
+	load_type int4 NOT NULL,
+	src_tbl_name varchar(50) NOT NULL,
+	trg_tbl_name varchar(50) NULL,
+	last_load_dttm timestamp NULL,
+	last_load_status varchar(50) NULL,
+	last_load_cnt int2 NULL,
+	message_text varchar(512) NULL,
+	crt_date timestamp NOT NULL DEFAULT clock_timestamp(),
+	sql_query text NULL,
+	CONSTRAINT tbl_load_update_pkey PRIMARY KEY (id)
+)
+WITH (
+	appendonly=false
+)
+DISTRIBUTED REPLICATED;
---
+++ more functions/add_columns_dlk.sql
@@ -0,0 +1,44 @@
+-- DROP FUNCTION public.add_columns_dlk();
+
+CREATE OR REPLACE FUNCTION public.add_columns_dlk()
+	RETURNS text
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+DECLARE     
+     cur refcursor;
+     l_table_name text;
+     l_table_schema text;
+     sql_query text;
+     
+begin
+	
+	open cur for execute 'select table_schema, table_name from information_schema.tables
+					  where 1= 1 and
+					  table_schema in (''stg'', ''gl'') and 
+                      table_type = ''BASE TABLE'' and 
+                      table_name not like ''%_prt_%'' and
+					  table_name like ''%dlk%''';
+
+	loop
+		fetch cur into l_table_schema, l_table_name;
+		
+		exit when not found;
+		begin
+			sql_query := 'alter table '|| l_table_schema || '.' || l_table_name ||' add column op_type varchar(1)';
+			execute sql_query;
+		
+			exception when others then
+		        raise notice '%', sql_query;
+		    	raise notice 'Такое поле уже существует в таблице: %', l_table_name;
+		end;
+	end loop;
+	
+   return 'Процесс завершен';
+END;
+
+
+$$
+EXECUTE ON ANY;
