f66c3a8c4b360f1f19ebcb5519f7d5c5aa46d6ca
Dmitry Santalov <dmitsantalov@yandex.ru>
2023-12-07 12:44:40 +0300

    rename functions

---
+++ more functions/rename_columns_system.sql
@@ -0,0 +1,43 @@
+-- DROP FUNCTION public.rename_column_system();
+
+CREATE OR REPLACE FUNCTION public.rename_column_system()
+	RETURNS varchar
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+DECLARE     
+
+cur refcursor;
+l_name_table text;
+l_column_name text;
+l_schema_name text;
+begin
+
+		open cur for execute 'select table_schema, table_name, column_name from information_schema.columns
+							  where column_name in (''w$ldate'', ''w$loadid'') and table_name not like ''%_prt_%''';	
+		loop 
+			fetch cur into l_schema_name, l_name_table, l_column_name;
+			exit when not found;
+			begin 
+				if l_column_name = 'w$ldate'
+					then 
+						execute 'alter table ' || l_schema_name || '.' || l_name_table || ' rename column w$ldate to w_ldate';
+				elsif l_column_name = 'w$loadid'
+					then 
+						execute 'alter table ' || l_schema_name || '.' || l_name_table || ' rename column w$loadid to w_loadid';
+				end if;
+				
+				--exception when others then
+					--raise notice 'Не получилось преобразовать поле таблицы %', l_name_table;
+			end;		
+				
+		end loop;
+
+return 'Операция завершена.';
+	
+END;
+
+$$
+EXECUTE ON ANY;
---
+++ more functions/rename_table_dollar.sql
@@ -0,0 +1,41 @@
+-- DROP FUNCTION testing.rename_table_dollar();
+
+CREATE OR REPLACE FUNCTION testing.rename_table_dollar()
+	RETURNS varchar
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+	
+DECLARE     
+
+cur refcursor;
+l_name_table_old text;
+l_name_table_new text;
+l_schema_name text;
+begin
+
+		open cur for execute 'select table_schema, table_name from information_schema.tables
+		where table_schema not in (''diskquota'', ''gp_toolkit'', ''pg_catalog'')
+		and table_name like ''%$%''';	
+		loop 
+			fetch cur into l_schema_name, l_name_table_old;
+			exit when not found;
+			begin 
+				
+				l_name_table_new := replace (l_name_table_old, '$', '_');
+				
+				execute 'alter table '|| l_schema_name || '.' || l_name_table_old || ' rename to ' || l_name_table_new;
+				
+				exception when others then
+					raise notice 'Не получилось преобразовать наименование таблицы %', l_name_table_old;
+			end;		
+				
+		end loop;
+
+return 'Операция завершена.';
+	
+END;
+
+$$
+EXECUTE ON ANY;
