d03dd04c840cdc17e4222d109d6e74f14ad245f4
Dmitry Santalov <dmitsantalov@yandex.ru>
2023-12-08 10:10:29 +0300

    Add files via upload

---
+++ f_cm_set_param.sql
@@ -0,0 +1,25 @@
+-- DROP FUNCTION meta_info.f_cm_set_param(varchar, varchar);
+
+CREATE OR REPLACE FUNCTION meta_info.f_cm_set_param(p_param_name varchar, p_param_value varchar)
+	RETURNS varchar
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+DECLARE     
+     l_qnt int2;
+     l_res varchar(200) := 'Устанволено значение: '||p_param_value||', параметра: '||p_param_name;
+begin
+   update meta_info.cm_prm_md set param_value = p_param_value
+   where param_name = p_param_name;
+   GET DIAGNOSTICS l_qnt = ROW_COUNT;
+  
+   if l_qnt = 0 then
+   	l_res := 'Параметр: '||p_param_name||' не найден';
+   end if;
+  
+   return l_res;
+END;
+
+$$
+EXECUTE ON ANY;
---
+++ f_ecm_set_tasks.sql
@@ -0,0 +1,40 @@
+-- DROP FUNCTION meta_info.f_ecm_set_tasks();
+
+CREATE OR REPLACE FUNCTION meta_info.f_ecm_set_tasks()
+	RETURNS text
+	LANGUAGE plpgsql
+	VOLATILE
+AS $$
+	
+declare
+	l_parent_id int;
+begin
+	delete from meta_info.ecm_task where process_type = 'STG_TO_GL';
+	insert into meta_info.ecm_task (system_code, process_type,	md_table_name, md_id, task_code,
+		status,	message, is_enable,	last_dttm)
+	SELECT 'DRP','STG_TO_GL','EE_GL_MD',id,'select meta_info.f_ee_gl_replicate('''||id||''')' exec_path, 1, '', 1, clock_timestamp()
+	FROM meta_info.ee_gl_md
+	where is_enable = 1  ;
+	
+	insert into meta_info.ecm_task (system_code, process_type,	md_table_name, md_id, task_code,
+		status,	message, is_enable,	last_dttm)
+	SELECT 'DRP','SRC_TO_STG','<NONE>',0,'<NONE>', 5, '', 0, clock_timestamp() 
+	where
+	not exists (select 1 from meta_info.ecm_task where process_type = 'SRC_TO_STG');
+	
+	truncate table  meta_info.ecm_task_link;
+    select max(id) into l_parent_id  from meta_info.ecm_task where process_type = 'SRC_TO_STG';
+	insert into meta_info.ecm_task_link (task_id, parent_id,is_enable)
+	select id, null,5  from meta_info.ecm_task where process_type = 'SRC_TO_STG';
+	
+	insert into meta_info.ecm_task_link (task_id, parent_id,is_enable)
+	select id, l_parent_id,1  
+	from meta_info.ecm_task where process_type = 'STG_TO_GL';
+    return 'SUCCESSFUL';
+exception when others then
+	return 'FAIL';
+END;
+
+
+$$
+EXECUTE ON ANY;
