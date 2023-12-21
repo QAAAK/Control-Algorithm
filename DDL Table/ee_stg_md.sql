9afb6e673f15fb0e8de48bc52f79fa56ac133883
Dmitry Santalov <dmitsantalov@yandex.ru>
2023-12-21 13:24:35 +0300

    new version table

---
+++ DDL Table/ee_stg_md.sql
@@ -0,0 +1,24 @@
+-- meta_info.ee_stg_md определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.ee_stg_md;
+
+CREATE TABLE meta_info.ee_stg_md (
+	id bigserial NOT NULL,
+	rss_code varchar(4) NOT NULL,
+	src_schema_name varchar(20) NOT NULL,
+	src_tbl_name varchar(20) NOT NULL,
+	task_code varchar(1000) NOT NULL,
+	date_from date NULL,
+	date_to date NULL,
+	stg_tbl_name varchar(50) NULL,
+	is_enable int2 NOT NULL,
+	last_load_dttm timestamp NULL,
+	last_load_status varchar(50) NULL,
+	last_load_cnt int4 NULL,
+	message_text varchar(512) NULL,
+	crt_date timestamp NOT NULL DEFAULT clock_timestamp(),
+	CONSTRAINT pk_ee_stg_md PRIMARY KEY (id) DEFERRABLE INITIALLY DEFERRED
+)
+DISTRIBUTED REPLICATED;
--- DDL Table/esm_task_link.sql
+++ DDL Table/esm_task_link.sql
@@ -1,16 +1,26 @@
--- meta_info.ecm_task_link определение
+-- meta_info.ecm_task определение
 
 -- Drop table
 
--- DROP TABLE meta_info.ecm_task_link;
+-- DROP TABLE meta_info.ecm_task;
 
-CREATE TABLE meta_info.ecm_task_link (
+CREATE TABLE meta_info.ecm_task (
 	id bigserial NOT NULL,
-	task_id int4 NOT NULL,
-	parent_id int4 NULL,
+	system_code varchar(30) NOT NULL,
+	process_type varchar(30) NOT NULL,
+	md_table_name varchar(30) NOT NULL,
+	md_id int2 NULL,
+	task_code varchar(1000) NOT NULL,
+	status int2 NOT NULL,
+	message varchar(1000) NULL,
 	is_enable int2 NOT NULL,
+	last_dttm timestamp NULL,
 	crt_dttm timestamp NOT NULL DEFAULT clock_timestamp(),
-	CONSTRAINT pk_ecm_link_queue PRIMARY KEY (id)
+	task_type text NULL,
+	prm_date_from date NULL,
+	prm_date_to date NULL,
+	last_load_cnt int4 NULL,
+	CONSTRAINT pk_ecm_queue PRIMARY KEY (id)
 )
 WITH (
 	appendonly=false
