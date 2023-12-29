831dc8cb0e7d74b098e908c67f4e4e8d05d49694
Dmitry Santalov <dmitsantalov@yandex.ru>
2023-12-29 10:28:29 +0300

    ver 2.0.

--- f_ee_reverse_status.sql
+++ f_ee_reverse_status.sql
@@ -1,16 +1,19 @@
--- DROP FUNCTION meta_info.f_ee_reverse_status(text, int2);
+-- DROP FUNCTION meta_info.f_ini(text, int2);
 
-CREATE OR REPLACE FUNCTION meta_info.f_ee_reverse_status(p_day text DEFAULT '1'::text, p_force int2 DEFAULT 0)
+CREATE OR REPLACE FUNCTION meta_info.f_ini(p_day text DEFAULT '1'::text, p_force int2 DEFAULT 0)
 	RETURNS text
 	LANGUAGE plpgsql
 	VOLATILE
 AS $$
 	
 	
+	
+
+		
 declare
 
 	l_id smallint; -- id таблицы
-	l_status smallint; -- статус 
+	l_status varchar(10); -- статус 
 	l_sql_query text; -- команда sql
 	l_days text := p_day || ' day'; -- интервал сдвига
 	l_time time := current_time; -- текущее время
@@ -19,39 +22,45 @@ declare
 	
 	begin
 		
-		if p_force = 0 and l_time > '04:00:00'
-			then 
-				return 'Некорректное время для запуска';
-		end if;
+		--if p_force = 0 and l_time > '04:00:00'
+			--then 
+				--return 'Некорректное время для запуска';
+		--end if;
 
-		open cur for execute 'select id, status from meta_info.ecm_task';
+	
+	   	
+		open cur for execute 'select id,  last_load_status from meta_info.ee_stg_md esm 
+							  union all
+							  select id, last_load_status from meta_info.ee_gl_md egm ';
 		loop
-			fetch cur into l_id, l_status;
+			fetch cur into l_id,  l_status;
 			exit when not found;
 			
 			begin
 		
-				if l_status = 5
+				if l_status = 'SACCESFUL' or l_status = 'FAIL'
 					then 
 						l_sql_query := 'update meta_info.ecm_task 
 										set status = 1, 
 										prm_date_from = prm_date_from + interval  ''' || l_days || ''' , 
 										prm_date_to = prm_date_to + interval  ''' || l_days || '''
-										where id = ' || l_id ;
+										where md_id = ' || l_id ;
+									
 						execute l_sql_query;
-				end if;
-					
+				end if; 
 				
 				exception when others then
-					raise notice 'Изменение не выполнено, ошибка в таблице с id: %', l_id;
+					raise notice 'Изменение не выполнено, ошибка в таблице с md_id: %', l_id;
 			 
 			end;
 		end loop;
-	
-		return 'Операция выполнена';	
-		
+			perform meta_info.f_ecm_set_tasks();
+		return 'Операция выполнена';		
 	end;
 			
+
+
+
 
 $$
 EXECUTE ON ANY;
