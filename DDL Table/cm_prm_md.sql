e7f590c413f50be5c93f0075997fe05c49564368
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-01-09 11:29:39 +0300

    ver 2.0

---
+++ DDL Table/cm_prm_md.sql
@@ -0,0 +1,20 @@
+-- meta_info.cm_prm_md определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.cm_prm_md;
+
+CREATE TABLE meta_info.cm_prm_md (
+	id serial4 NOT NULL,
+	param_name varchar(30) NOT NULL,
+	param_value varchar(30) NULL,
+	param_value_def varchar(30) NULL,
+	is_enable int4 NULL,
+	description varchar(500) NOT NULL,
+	crt_date timestamp NULL DEFAULT clock_timestamp(),
+	CONSTRAINT pk_cm_prm_md PRIMARY KEY (id) DEFERRABLE INITIALLY DEFERRED
+)
+WITH (
+	appendonly=false
+)
+DISTRIBUTED REPLICATED;
---
+++ DDL Table/ee_rss_md.sql
@@ -0,0 +1,29 @@
+-- meta_info.ee_rss_md определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.ee_rss_md;
+
+CREATE TABLE meta_info.ee_rss_md (
+	id int4 NOT NULL,
+	rss_code varchar(4) NOT NULL,
+	rss_name varchar(500) NOT NULL,
+	rss_description varchar(500) NULL,
+	data_integrator varchar(30) NULL,
+	priority int2 NOT NULL,
+	is_enable int2 NOT NULL,
+	crt_date timestamp NULL DEFAULT clock_timestamp(),
+	"host" varchar(50) NULL,
+	port int2 NULL,
+	login varchar(30) NULL,
+	"password" varchar(30) NULL,
+	rss_schema varchar(30) NULL,
+	rss_type varchar(30) NULL,
+	interaction_type varchar(30) NULL,
+	rss_db varchar(30) NULL,
+	CONSTRAINT pk_ee_rss_md PRIMARY KEY (id) DEFERRABLE INITIALLY DEFERRED
+)
+WITH (
+	appendonly=false
+)
+DISTRIBUTED REPLICATED;
