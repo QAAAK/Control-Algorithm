64017498feae949b30524f04d6f11ef81c9a94f5
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-01-10 14:26:51 +0300

    ver 2.1

---
+++ DDL Table/log_func.sql
@@ -0,0 +1,14 @@
+-- meta_info.log_func определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.log_func;
+
+CREATE TABLE meta_info.log_func (
+	id int8 NOT NULL DEFAULT nextval('meta_info.func_id_seq'::regclass),
+	func text NOT NULL,
+	status text NULL,
+	return_value text NULL,
+	last_launch_dttm timestamp NOT NULL DEFAULT now()
+)
+DISTRIBUTED REPLICATED;
