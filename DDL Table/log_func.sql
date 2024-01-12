03acb6b9ab41dd53a1d11fcb42b9955d37d2ac91
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-01-12 15:23:19 +0300

    ver 2.1.1

--- DDL Table/log_func.sql
+++ DDL Table/log_func.sql
@@ -8,7 +8,7 @@ CREATE TABLE meta_info.log_func (
 	id int8 NOT NULL DEFAULT nextval('meta_info.func_id_seq'::regclass),
 	func text NOT NULL,
 	status text NULL,
-	return_value text NULL,
+	detail text NULL,
 	last_launch_dttm timestamp NOT NULL DEFAULT now()
 )
 DISTRIBUTED REPLICATED;
