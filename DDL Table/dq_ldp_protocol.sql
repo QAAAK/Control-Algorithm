53311fd7025588baa02c697c21ca7b69f7116f1c
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-01-26 16:04:41 +0300

    ver 2.0.

---
+++ DDL Table/dq_ldp_protocol.sql
@@ -0,0 +1,23 @@
+-- meta_info.dq_ldp_protocol определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.dq_ldp_protocol;
+
+CREATE TABLE meta_info.dq_ldp_protocol (
+	id bigserial NOT NULL,
+	trg_tbl_name varchar NULL,
+	check_type varchar NULL,
+	date_start timestamp NULL,
+	date_end timestamp NULL,
+	loss_cnt numeric NULL,
+	status varchar NULL,
+	recovery_dttm timestamp NULL,
+	crt_date timestamp NULL DEFAULT now()
+)
+DISTRIBUTED REPLICATED;
+
+-- Permissions
+
+ALTER TABLE meta_info.dq_ldp_protocol OWNER TO drp;
+GRANT ALL ON TABLE meta_info.dq_ldp_protocol TO drp;
---
+++ DDL Table/dq_ldp_protocol_detail.sql
@@ -0,0 +1,18 @@
+-- meta_info.dq_ldp_protocol_detail определение
+
+-- Drop table
+
+-- DROP TABLE meta_info.dq_ldp_protocol_detail;
+
+CREATE TABLE meta_info.dq_ldp_protocol_detail (
+	id bigserial NOT NULL,
+	protocol_id numeric NOT NULL,
+	loss_id varchar NULL,
+	crt_dttm timestamp NULL DEFAULT now()
+)
+DISTRIBUTED REPLICATED;
+
+-- Permissions
+
+ALTER TABLE meta_info.dq_ldp_protocol_detail OWNER TO drp;
+GRANT ALL ON TABLE meta_info.dq_ldp_protocol_detail TO drp;
